33f1c4473eb73e833a12ffd28f329f0669e5b56d
yeshiwasT <yeshiwastikimu0@gmail.com>
2023-06-15 10:43:53 +0300

    adding telegram bot api

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const { convertToAmharic }=require('amharic-converter');
 const express = require('express')
+const TelegramBot = require('node-telegram-bot-api');
 const app = express()
 const port = process.env.PORT ||3000
 
+const token = '6285301960:AAFGIFv8ggOGI44lXPX6usml8Lkm4gIUJHA';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// // Matches "/echo [whatever]"
+// bot.onText(/\/echo (.+)/, (msg, match) => {
+//   // 'msg' is the received Message from Telegram
+//   // 'match' is the result of executing the regexp above on the text content
+//   // of the message
+
+//   const chatId = msg.chat.id;
+//   const resp = match[1]; // the captured "whatever"
+
+//   // send back the matched "whatever" to the chat
+//   bot.sendMessage(chatId, msg);
+// });
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+console.log(msg);
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
 app.get('/amharic', (req, res) => {
 
     const amharicResult = convertToAmharic("Mnm alatefahm ene gn normal mehon alichalkum kayewuk bohala kelela gizew belay kante gar mhon eyfelku nw yh dgmo lik aydlm Tilk sew nesh enem endezaw demo mnm endemanidebabek neber masibew beza lay mnm tenegagiren meftat miyakiten neger aynorm drsha pls anch endih stihognibgn dess ayilm", {
@@ -16,6 +43,7 @@ res.send(amharicResult)
 app.get('/', (req, res) => { 
     res.send('Hello World!')
   })
+  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
